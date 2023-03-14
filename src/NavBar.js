5593c4e43684ed05820f59c849981cf80416d242
support <support@kmcintosh-6782.remax.local>
2023-03-14 14:04:58 -0600

    added NavBar.js

---
+++ src/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+
+function NavBar() {
+    return (
+        <div className="navbar">
+            <NavLink exact to="/">Home</NavLink>
+            <br/>
+            <NavLink exact to="/locations">Locations</NavLink>
+            <br/>
+            {/* <NavLink to="/venues">All Venues</NavLink>
+            <br/> */}
+            <NavLink to="/venues/new">Add Venue</NavLink>
+            <br/>
+            <NavLink exact to="/locations/new">Add Location</NavLink>
+            <br/>
+            <NavLink to="/about">About</NavLink>
+        </div>
+    )
+}
+
+export default NavBar;
