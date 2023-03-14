eea5b09153c5f32a6c960048665f359a4b4669f4
support <support@kmcintosh-6782.remax.local>
2023-03-14 13:58:31 -0600

    added Header.js

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header({ isDarkMode, onDarkModeClick}) {
+
+  return (
+    <header>
+      <br />
+      <strong><h1>Indoor Colorado Concert Venues</h1></strong>
+    </header>
+  );
+}
+
+export default Header;
