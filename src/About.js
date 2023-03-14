8dd631f7183e6b54915728fa8cc02cdae7d47820
support <support@kmcintosh-6782.remax.local>
2023-03-14 13:57:58 -0600

    added About.js

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+    return (
+        <div>
+            <p>This Application is designed to give insight into the various wonderful indoor concert venues in Colorado. <br />
+                This Single Page Application was created by Kevin McIntosh for the Flatiron School Software Engineer Flex Program.</p>
+        </div>
+    )
+}
+
+export default About;
