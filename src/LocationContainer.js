d608a903290c5b5817249976b2b229469d739342
support <support@kmcintosh-6782.remax.local>
2023-03-14 14:04:24 -0600

    added LocationContainer.js

---
+++ src/LocationContainer.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from "react";
+import Search from "./Search";
+import LocationCard from "./LocationCard";
+//this component is where we store our locations
+function LocationContainer({locations}) {
+
+  
+////new array that filters through if name, description or location matches the search terms. Also handles case insensitive matches
+
+      // const filteredLocations = locations.filter(location => {
+      //   return (location.city.toLowerCase().includes(search.toLowerCase())) 
+      //   || location.state.toLowerCase().includes(search.toLowerCase())
+      // })
+
+      
+////iterating over the filtered array and creating a separate LocationCard for each location
+      const locationCards = locations.map(location => {
+        return <LocationCard 
+          key={location.id} 
+          location={location} 
+          />
+      })
+      
+
+    return (
+        <div>
+          <h1>Locations:</h1>
+                {locationCards}
+        </div>
+    )
+}
+
+export default LocationContainer;
