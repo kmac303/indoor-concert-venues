c2fe13fa777bed615617f56f2168e36c85c74088
support <support@kmcintosh-6782.remax.local>
2023-03-20 12:55:11 -0600

    updated Home page description

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ function Home() {
       <img src="https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/327912548_1514101362448447_3580270556796621639_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=kH6Mng__dK4AX8gpU40&_nc_ht=scontent-den4-1.xx&oh=00_AfDmSSkyzg1QzvSNFD7nj5Xt70c8xRZFLWrAJ8goWbLl9g&oe=64187D36" 
       alt="Denver"/>
         <p>
-          The state of Colorado has some of the best indoor concert venues. This app is designed to keep track of them all.
+          The state of Colorado has some of the best indoor concert venues in the world. This app is designed to keep track of them all.
         </p>
       <Button href="http://localhost:3000/locations"variant="primary" size="lg">
         See Locations
